6a6ab02f558e8fde816fbc6942261757adb124d1
Zeeshan <155964871+zeeshanOT@users.noreply.github.com>
2024-02-24 13:40:46 +0500

    updated

--- src/utils/config.js
+++ src/utils/config.js
@@ -47,7 +47,7 @@ const getServerConfiguration = (version) => {
 	const development = {
 		version: CONST.RUN_MODE.DEVELOPMENT,
 		FURL: "http://192.168.100.133:3000",
-		URL: "https://52.35.148.228:443	", // API endpoint
+		URL: "http://52.35.148.228:443	", // API endpoint
 		CLOUD_IMAGE: "https://dbvegu4yzhf6f.cloudfront.net",
 		RUNNING: "LOCAL"
 	}
